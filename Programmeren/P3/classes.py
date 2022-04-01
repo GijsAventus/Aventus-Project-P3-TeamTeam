c8bf84c1d0d617d8fcca88d8cda833affc5ed2a3
GijsAventus <gijs.bal@student.aventus.nl>
2022-04-01 11:08:10 +0200

    Create classes.py

---
+++ Programmeren/P3/classes.py
@@ -0,0 +1,17 @@
+#dit bestand gebruiken voor classes
+
+
+#class voor opdracht 1
+class calculate:
+    def __init__(self) -> None:
+        pass
+
+#class voor opdracht 2
+class relax:
+    def __init__(self) -> None:
+        pass
+
+#class voor opdracht 3
+class xxx: #voor opdracht 3
+    def __init__(self) -> None:
+        pass

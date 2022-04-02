42a24f23aeabf0a6f66c5400c17a1a6104eb6714
GijsAventus <gijs.bal@student.aventus.nl>
2022-04-02 10:19:02 +0200

    Update classes.py

--- Programmeren/P3/classes.py
+++ Programmeren/P3/classes.py
@@ -1,10 +1,30 @@
 #dit bestand gebruiken voor classes (logica & functies)
+from math import pi
 
 
 #class voor opdracht 1
 class calculate:
-    def __init__(self) -> None:
-        pass
+    def btw(num):
+        return (num * 1.21) - num
+    
+    def reistijd(afstand, snelheid):
+        return f"De reistijd is {str(afstand / snelheid)}"
+
+    def vierkant(lengte, breedte):
+        oppervlakte = lengte * breedte
+        omtrek = (lengte + breedte) * 2
+        string1 = f"Het vierkant heeft een oppervlakte van {oppervlakte} en een omtrek van {omtrek}."
+        return string1
+
+    def cirkel(diameter):
+        oppervlakte = ((oppervlakte/2) ** 2) * pi
+        omtrek = diameter * pi
+        return f"Deze cirkel heeft een oppervlakte van {oppervlakte} en een omtrek van {omtrek}."
+
+    def procent(num1, num2):
+        pct = (num1 / num2) * 100
+        return f"Het getal 1 is {pct} procent groot als getal 2."
+        
 
 #class voor opdracht 2
 class relax:
@@ -15,3 +35,5 @@ class relax:
 class xxx:
     def __init__(self) -> None:
         pass
+
+print(calculate.cirkel(5))

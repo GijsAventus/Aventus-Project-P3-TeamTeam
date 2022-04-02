6f822c5263ec5389ef4b865e44d8acb53fea5dfe
GijsAventus <99733828+GijsAventus@users.noreply.github.com>
2022-04-02 11:41:36 +0200

    Update classes.py

--- Programmeren/P3/classes.py
+++ Programmeren/P3/classes.py
@@ -4,23 +4,29 @@ from math import pi
 
 #class voor opdracht 1
 class calculate:
+    #functie voor 1.1
     def btw(num):
-        return (num * 1.21) - num
+        btw = round(num * 0.21, 2)
+        return f"De btw is {btw}."
     
+    #functie voor 1.2
     def reistijd(afstand, snelheid):
         return f"De reistijd is {str(afstand / snelheid)}"
 
+    #functie voor 1.3
     def vierkant(lengte, breedte):
         oppervlakte = lengte * breedte
         omtrek = (lengte + breedte) * 2
         string1 = f"Het vierkant heeft een oppervlakte van {oppervlakte} en een omtrek van {omtrek}."
         return string1
 
+    #functie voor 1.4
     def cirkel(diameter):
-        oppervlakte = ((oppervlakte/2) ** 2) * pi
+        oppervlakte = ((diameter/2) ** 2) * pi
         omtrek = diameter * pi
-        return f"Deze cirkel heeft een oppervlakte van {oppervlakte} en een omtrek van {omtrek}."
+        return f"De cirkel heeft een oppervlakte van {str(oppervlakte)[0:5]} en een omtrek van {str(omtrek)[0:5]}."
 
+    #functie voor 1.5
     def procent(num1, num2):
         pct = (num1 / num2) * 100
         return f"Het getal 1 is {pct} procent groot als getal 2."
@@ -35,5 +41,3 @@ class relax:
 class xxx:
     def __init__(self) -> None:
         pass
-
-print(calculate.cirkel(5))
